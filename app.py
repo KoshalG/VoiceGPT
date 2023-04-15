84be63af28720e76bfcd647cfdc2eed97b8aa685
KoshalG <87081596+KoshalG@users.noreply.github.com>
2023-04-15 15:25:23 +0530

    Added audio recording in JS and modified app.py
    Added audio recording functionality in JS and made changes in the python script.

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ import openai
 import warnings
 import io
 
-warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore") # ignore warnings
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route("/transcribe", methods=["POST"])
+@app.route("/transcribe", methods=["POST"]) # POST request to /transcribe
 def transcribe():
     # transcribes the audio data from the request
-    audio_data = request.data
-    transcript = openai.Audio.transcribe("whisper-1", audio_data)
-    return transcript
+    audio_data = request.files.get("file")
+    transcript = openai.Audio.transcribe("whisper-1", audio_data.read())
+    
+    # generate response from OpenAI GPT-3 using transcribed text as prompt
+    messages = [{"role": "user", "content": transcript}]
+    chat_completion = openai.Completion.create(model="chatgpt-3.5-turbo", messages=messages)
+    reply = chat_completion.choices[0].text
 
-@app.route("/tts", methods=["POST"])
-def tts():
-    # converts text to speech using gTTS
-    text = request.data.decode()
-    audio_obj = gTTS(text=text, lang="en", slow=False)
+    # convert text response to audio
+    audio_obj = gTTS(text=reply, lang="en", slow=False)
     audio_data = io.BytesIO()
     audio_obj.write_to_fp(audio_data)
     audio_data.seek(0)
@@ -30,19 +31,6 @@ def tts():
         'Content-Disposition': 'attachment; filename="audio.mp3"'
     }
     return Response(audio_data, headers=headers)
-
-@app.route("/chat", methods=["POST"])
-def chat():
-    # uses OpenAI GPT-3 to generate a response
-    messages = [{"role": "system", "content": "You are a helpful assistant."}]
-    input_text = request.data.decode()
-    if input_text:
-        messages.append({"role": "user", "content": input_text})
-        chat_completion = openai.Completion.create(model="chatgpt-3.5-turbo", messages=messages)
-        reply = chat_completion.choices[0].text
-    else:
-        reply = "I didn't get that. Can you try again?"
-    return reply
 
 if __name__ == '__main__':
     app.run()
---
+++ script.js
@@ -0,0 +1,83 @@
+const recordBtn = document.getElementById("record-btn"); // HTML button
+const statusElement = document.querySelector(".status"); // HTML div
+let recorder = null;
+let isRecording = false;
+let isProcessing = false;
+
+recordBtn.addEventListener("click", async () => {
+  // If we are already recording, stop it
+  if (!isRecording) {
+    isRecording = true;
+    isProcessing = false;
+    statusElement.innerHTML = "Audio being recorded";
+
+    // Start recording
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      recorder = new MediaRecorder(stream, { mimeType: "audio/webm" });
+
+      const chunks = [];
+      recorder.addEventListener("dataavailable", (event) => {
+        chunks.push(event.data);
+      });
+
+      recorder.start();
+
+      let silenceTimeout = null;
+      recorder.addEventListener("start", () => {
+        silenceTimeout = setTimeout(() => {
+          recorder.stop();
+        }, 5000);
+      });
+
+      recorder.addEventListener("stop", () => {
+        clearTimeout(silenceTimeout);
+
+        const blob = new Blob(chunks, { type: "audio/webm" });
+        sendAudio(blob);
+      });
+    } catch (error) {
+      console.error(error);
+      statusElement.innerHTML = "Failed to start recording";
+      isRecording = false;
+      isProcessing = false;
+    }
+  }
+});
+
+// Send audio to server
+function sendAudioToServer(blob) {
+  statusElement.innerHTML = "Processing";
+  fetch("/transcribe", {
+    method: "POST",
+    body: blob,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Server responded with ${response.status}`);
+      }
+      return response.blob();
+    })
+    .then((audioBlob) => {
+      playAudio(audioBlob);
+      statusElement.innerHTML = "Playing audio";
+    })
+    .catch((error) => {
+      console.error(error);
+      statusElement.innerHTML = "Failed to process audio";
+      isRecording = false;
+      isProcessing = false;
+    });
+}
+
+// Play audio
+function playAudio(blob) {
+  const audio = new Audio(URL.createObjectURL(blob));
+  audio.addEventListener("ended", () => {
+    statusElement.innerHTML = "Click the button to start recording";
+    isRecording = false;
+    isProcessing = false;
+  });
+
+  audio.play();
+}
